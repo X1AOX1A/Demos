631f4e63112da5c85e5e4c653d851fb91e942ff0
X1AOX1A <1764400017@e.gzhu.edu.cn>
2023-06-01 17:49:38 +0800

    add file for source installation

---
+++ MiniGPT-4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_namespace_packages
+setup(
+    name='minigpt4',
+    version='1.0',
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='A brief description of your project',
+    install_requires=[],
+    packages=find_namespace_packages(include="minigpt4.*"),
+)
