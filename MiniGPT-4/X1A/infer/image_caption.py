f77bd64ca1041a3f45cbb6f5b2ce48d895d4a417
X1AOX1A <1764400017@e.gzhu.edu.cn>
2023-06-02 11:11:34 +0800

    run VQA/image caption inference

--- MiniGPT-4/X1A/infer/image_caption.py
+++ MiniGPT-4/X1A/infer/image_caption.py
@@ -1,15 +1,22 @@
 import argparse
-from PIL import Image
+import torch.backends.cudnn as cudnn
+
 from minigpt4.common.config import Config
 from minigpt4.common.registry import registry
+from minigpt4.conversation.conversation import Chat, CONV_VISION
+
+# imports modules for registration
+from minigpt4.datasets.builders import *
+from minigpt4.models import *
+from minigpt4.processors import *
+from minigpt4.runners import *
+from minigpt4.tasks import *
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Image Caption Inference")
-    parser.add_argument(
-        "--cfg-path", 
-        default="/root/Documents/DEMOS/MiniGPT-4/eval_configs/minigpt4_eval.yaml", 
-        help="path to configuration file.")
+    parser = argparse.ArgumentParser(description="Demo")
+    parser.add_argument("--cfg-path", help="path to configuration file.")
+    parser.add_argument("--gpu-id", type=int, default=0, help="specify the gpu to load the model.")
     parser.add_argument(
         "--options",
         nargs="+",
@@ -21,27 +28,42 @@ def parse_args():
     return args
 
 
-if __name__ == "__main__":
-    # Input
-    IMAGE_PATH = "/root/Documents/DEMOS/OFA/X1A/inference/cat.jpg"
-    PROMPT = "Write a detailed description."
-    GPU_ID = 0
-
+def init_model(args):
     print('Initializing Model...')
-
-    # prepare the config
-    args = parse_args()
     cfg = Config(args)
-    model_config = cfg.model_cfg
-    model_config.device_8bit = GPU_ID
-    model_cls = registry.get_model_class(model_config.arch)
 
-    # prepare the model
-    model = model_cls.from_config(model_config).to('cuda:{}'.format(GPU_ID))
+    model_config = cfg.model_cfg
+    model_config.device_8bit = args.gpu_id
+    model_cls = registry.get_model_class(model_config.arch)
+    model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
+
     vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
     vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
+    chat = Chat(model, vis_processor, device='cuda:{}'.format(args.gpu_id))
+    print('Initialization Finished')
+    return chat
 
-    # prepare the image
-    raw_image = Image.open(IMAGE_PATH).convert("RGB")
-    image = vis_processor["eval"](raw_image).unsqueeze(0).to('cuda:{}'.format(GPU_ID))
-    print(model.generate({"image": image, "prompt": PROMPT}))
+
+def run_vqa(image, prompt, chat):
+    chat_state = CONV_VISION.copy()
+    img_list = []
+    llm_message = chat.upload_img(image, chat_state, img_list)
+    chat.ask(prompt, chat_state)
+    llm_message = chat.answer(conv=chat_state, img_list=img_list, num_beams=1, temperature=1.0, max_new_tokens=300, max_length=2000)[0]
+    return llm_message
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    args.cfg_path = "/root/Documents/DEMOS/MiniGPT-4/eval_configs/minigpt4_eval.yaml"
+    args.gpu_id = 0
+
+    # Inputs
+    IMAGE_PATH = "/root/Documents/DEMOS/OFA/X1A/inference/cat.jpg"
+    PROMPT = "Describe this image in detail."
+
+    # Run VQA
+    chat = init_model(args)
+    output = run_vqa(IMAGE_PATH, PROMPT, chat)
+    print(output)
+    # This is an image of a small cat peeking out from under a bed. It appears to be a domestic shorthair cat with grey and white fur, standing on its hind legs and looking up at the camera. It has bright blue eyes and a small, pointed face. The cat is standing on a wooden bed frame, with a blanket draped over it. The background appears to be a wall with a white sheet hanging on it. The lighting in the room is soft and diffused, casting a warm glow on the cat's fur.
