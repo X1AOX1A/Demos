374358e25eb8b2864f8a4669d87b598dc2281383
X1AOX1A <1764400017@e.gzhu.edu.cn>
2023-06-03 00:02:37 +0800

    add zeroshot evaluation

---
+++ MiniGPT-4/X1A/eval/caption_coco_zeroshot_vicuna7b.sh
@@ -0,0 +1,3 @@
+cd /root/Documents/DEMOS/MiniGPT-4
+nohup /root/anaconda3/envs/minigpt4/bin/python -m torch.distributed.run --nproc_per_node=2 evaluate.py --cfg-path /root/Documents/DEMOS/MiniGPT-4/X1A/eval/caption_coco_zeroshot_vicuna7b.yaml >/root/Documents/DEMOS/MiniGPT-4/X1A/eval/caption_coco_zeroshot_vicuna7b.log& 2>&1
+tail -f /root/Documents/DEMOS/MiniGPT-4/X1A/eval/caption_coco_zeroshot_vicuna7b.log
