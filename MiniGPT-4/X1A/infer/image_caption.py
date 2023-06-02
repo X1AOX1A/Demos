7f41262286260b53b104094a12f698fee4557915
X1AOX1A <1764400017@e.gzhu.edu.cn>
2023-06-02 19:09:16 +0800

    update file

--- MiniGPT-4/X1A/infer/image_caption.py
+++ MiniGPT-4/X1A/infer/image_caption.py
@@ -5,11 +5,9 @@ from minigpt4.common.registry import registry
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Image Caption Inference")
-    parser.add_argument(
-        "--cfg-path", 
-        default="/root/Documents/DEMOS/MiniGPT-4/eval_configs/minigpt4_eval.yaml", 
-        help="path to configuration file.")
+    parser = argparse.ArgumentParser(description="Demo")
+    parser.add_argument("--cfg-path", help="path to configuration file.")
+    parser.add_argument("--gpu-id", type=int, default=0, help="specify the gpu to load the model.")
     parser.add_argument(
         "--options",
         nargs="+",
@@ -20,28 +18,27 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-
-if __name__ == "__main__":
-    # Input
-    IMAGE_PATH = "/root/Documents/DEMOS/OFA/X1A/inference/cat.jpg"
-    # PROMPT = "Write a detailed description."
-    GPU_ID = 0
-
-    print('Initializing Model...')
-
-    # prepare the config
+if __name__ == "__main__":    
     args = parse_args()
+    args.cfg_path = "/root/Documents/DEMOS/MiniGPT-4/eval_configs/minigpt4_eval.yaml"
+    args.gpu_id = 0
+    
+    # Inputs
+    IMAGE_PATH = "/root/Documents/DEMOS/OFA/X1A/inference/cat.jpg"
+    # TEXT = "Describe this image in detail."
+    
+    print('Initializing Model...')
+    # prepare the model
     cfg = Config(args)
     model_config = cfg.model_cfg
-    model_config.device_8bit = GPU_ID
+    model_config.device_8bit = args.gpu_id
     model_cls = registry.get_model_class(model_config.arch)
 
-    # prepare the model
-    model = model_cls.from_config(model_config).to('cuda:{}'.format(GPU_ID))
+    model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
     vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
     vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
 
     # prepare the image
     raw_image = Image.open(IMAGE_PATH).convert("RGB")
-    image = vis_processor(raw_image).unsqueeze(0).to('cuda:{}'.format(GPU_ID))
+    image = vis_processor(raw_image).unsqueeze(0).to('cuda:{}'.format(args.gpu_id))
     print(model.generate({"image": image}))
