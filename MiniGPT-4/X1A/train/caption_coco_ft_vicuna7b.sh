c28ef86ef77f788f50219c4272b178cd5e7453f9
X1AOX1A <1764400017@e.gzhu.edu.cn>
2023-06-04 09:36:26 +0800

    add finetune file

---
+++ MiniGPT-4/X1A/train/caption_coco_ft_vicuna7b.sh
@@ -0,0 +1,3 @@
+cd /root/Documents/DEMOS/MiniGPT-4
+nohup /root/anaconda3/envs/minigpt4/bin/python -m torch.distributed.run --nproc_per_node=4 train.py --cfg-path /root/Documents/DEMOS/MiniGPT-4/X1A/train/caption_coco_ft_vicuna7b.yaml >/root/Documents/DEMOS/MiniGPT-4/X1A/output/train/caption_coco_ft_vicuna7b.log& 2>&1
+tail -f /root/Documents/DEMOS/MiniGPT-4/X1A/output/train/caption_coco_ft_vicuna7b.log
