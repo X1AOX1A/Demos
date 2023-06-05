38d050be1b506a3ea4f55e37c17bee89aad80d69
X1AOX1A <1764400017@e.gzhu.edu.cn>
2023-06-05 11:25:57 +0800

    update config

--- MiniGPT-4/X1A/demo/start_demo.sh
+++ MiniGPT-4/X1A/demo/start_demo.sh
@@ -1,2 +1,2 @@
 cd /root/Documents/DEMOS/MiniGPT-4
-/root/anaconda3/envs/minigpt4/bin/python demo.py --cfg-path eval_configs/minigpt4_eval.yaml  --gpu-id 0
+/root/anaconda3/envs/minigpt4/bin/python demo.py --cfg-path /root/Documents/DEMOS/MiniGPT-4/X1A/demo/demo_13b.yaml  --gpu-id 0
